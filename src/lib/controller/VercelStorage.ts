60e8e2e1f3f2e467fd2fc648e9c954ee4da1ab8f
e-goat <gorgonaly@gmail.com>
2026-02-24 23:34:04 +0200

    Add storeTemplate method to VercelStorage for handling file uploads with category validation

--- src/lib/controller/VercelStorage.ts
+++ src/lib/controller/VercelStorage.ts
@@ -1,46 +1,89 @@
-import { put } from '@vercel/blob'
-import type { PutBlobResult } from '@vercel/blob'
-import { BLOB_READ_WRITE_TOKEN } from '$lib/server/secrets'
+import { put } from "@vercel/blob";
+import type { PutBlobResult } from "@vercel/blob";
+import { BLOB_READ_WRITE_TOKEN } from "$lib/server/secrets";
 
 class VercelStorage {
-    async store({
+    async storeAudio({
         file,
         mimeType,
         uuid,
     }: {
-        file: File
-        mimeType: string
-        uuid: string
+        file: File;
+        mimeType: string;
+        uuid: string;
     }): Promise<PutBlobResult> {
         try {
             if (!file) {
-                throw new Error('Missing file')
+                throw new Error("Missing file");
             }
 
             if (!mimeType) {
-                throw new Error('Missing mime type')
+                throw new Error("Missing mime type");
             }
 
             if (!uuid) {
-                throw new Error('Missing filename')
+                throw new Error("Missing filename");
             }
 
             if (!BLOB_READ_WRITE_TOKEN) {
-                throw new Error('Missing vercel storage token')
+                throw new Error("Missing vercel storage token");
             }
 
             const result = await put(`${uuid}.${mimeType}`, file, {
-                access: 'public',
-            })
+                access: "public",
+            });
 
-            console.log('Recording stored successfully', result)
+            console.log("Recording stored successfully", result);
 
-            return result
+            return result;
         } catch (error) {
-            console.error('Recording storage failed', error)
-            throw error
+            console.error("Recording storage failed", error);
+            throw error;
+        }
+    }
+
+    async storeTemplate({
+        file,
+        mimeType,
+        category,
+    }: {
+        file: File;
+        mimeType: string;
+        category: string;
+    }): Promise<PutBlobResult> {
+        try {
+            if (!file) {
+                throw new Error("Missing file");
+            }
+
+            if (!mimeType) {
+                throw new Error("Missing mime type");
+            }
+
+            if (!category) {
+                throw new Error("Missing filename");
+            }
+
+            if (!BLOB_READ_WRITE_TOKEN) {
+                throw new Error("Missing vercel storage token");
+            }
+
+            const result = await put(
+                `${category}/${file.name}.${mimeType}`,
+                file,
+                {
+                    access: "public",
+                },
+            );
+
+            console.log("Image stored successfully", result);
+
+            return result;
+        } catch (error) {
+            console.error("Image storage failed", error);
+            throw error;
         }
     }
 }
 
-export const VercelStorageController = new VercelStorage()
+export const VercelStorageController = new VercelStorage();
