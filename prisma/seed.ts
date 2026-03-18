494d7aa5114b63c7c605526cd02a88c781ef01f3
e-goat <gorgonaly@gmail.com>
2026-03-18 15:28:18 +0200

    Prepare seed.ts for multi-table seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,15 +2,29 @@ import { PrismaClient, Prisma } from '../src/lib/db'
 
 const prisma = new PrismaClient()
 
+// Add tables to seed here
+const SEED_TARGET: string[] = [
+    // 'card',
+    // 'template',
+];
+
+/**
+ * @param data The data to seed into the table
+ * @param table The name of the table to seed
+ */
 export async function main(data: any, table: string) {
     switch (table) {
         case 'card':
+            // Seed card here
             break
         case 'template':
+            // Seed template here
             break
         default:
             throw new Error(`Unknown table: ${table}`)
     }
 }
 
-main(null, 'template')
+for (const table of SEED_TARGET) {
+    await main(null, table)
+}
